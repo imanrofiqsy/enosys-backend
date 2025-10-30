b685063d270cc62858199e7f8063d5b2ab1e7e18
imanrofiqsy <imrofsy31@gmail.com>
2025-10-30 09:22:45 +0700

    fix_15

--- bms_0n/consumers.py
+++ bms_0n/consumers.py
@@ -3,6 +3,15 @@ import json
 
 class MyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Nama grup harus sama dengan yang dipakai di kirim_data_dashboard()
+        self.group_name = "dashboard_group"
+
+        # Tambahkan koneksi ke grup
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        
         await self.accept()
         await self.send(text_data=json.dumps({"message": "Connected"}))
 
