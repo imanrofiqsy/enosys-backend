d6eee1be8cc7ae794c6f3a71d9346253de3deadd
imanrofiqsy <imrofsy31@gmail.com>
2025-10-31 13:48:16 +0700

    test domain_0

--- bms_0n/urls.py
+++ bms_0n/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from bms_0n import views
 
 urlpatterns = [
-    path('test/', views.dashboard, name='dashboard'),
+    path('', views.dashboard, name='dashboard'),
+    path('test/', views.test, name='test'),
 ]
--- bms_0n/views.py
+++ bms_0n/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse, JsonResponse
+import json
 
 def dashboard(request):
     return JsonResponse({"status": "ok", "message": "Backend Railway is running"})
 
+def test(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        print("data: ",data)
