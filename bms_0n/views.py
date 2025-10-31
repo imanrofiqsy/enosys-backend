d4161c7742cef4d164d245e134f76464a408c238
imanrofiqsy <imrofsy31@gmail.com>
2025-10-31 14:50:25 +0700

    fix typo_2

--- bms_0n/views.py
+++ bms_0n/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+import logging
 
 def dashboard(request):
     return JsonResponse({"status": "ok", "message": "Backend Railway is running"})
@@ -9,7 +10,7 @@ def dashboard(request):
 def test(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        print("data: ",data)
+        logging.print("data: ",data)
     forwarded = request.META.get('HTTP_X_FORWARDED_FOR')
     remote = request.META.get('REMOTE_ADDR')
     return JsonResponse({
