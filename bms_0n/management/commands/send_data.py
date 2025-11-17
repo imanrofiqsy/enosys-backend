250a7c9c276b879b4e72c9ef54821d27226eb89a
imanrofiqsy <imrofsy31@gmail.com>
2025-11-17 13:12:02 +0700

    debug_31

--- bms_0n/management/commands/send_data.py
+++ bms_0n/management/commands/send_data.py
@@ -238,7 +238,7 @@ class Command(BaseCommand):
                             hourly_kwh: r._value_l - r._value_f
                     }}))
                     |> group(columns: ["_time"])
-                    |> sum(column: "_value")
+                    |> sum(column: "_kwh")
                     '''
 
 
