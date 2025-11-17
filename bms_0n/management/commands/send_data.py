343c7c6f79d1abe75a92df9dcf3e494d84732b4b
imanrofiqsy <imrofsy31@gmail.com>
2025-11-17 09:54:55 +0700

    debug_9

--- bms_0n/management/commands/send_data.py
+++ bms_0n/management/commands/send_data.py
@@ -442,7 +442,7 @@ from(bucket: "{BUCKET}")
                     send("weekly_chart", weekly_chart)
                     send("overview_room", overview_data)
                     send("system_status", system_status)
-                    send("ping", record_value)
+                    send("ping", {"value": record_value})
 
                 except Exception as e:
                     logger.exception("Failed building/sending dashboard payload: %s", e)
