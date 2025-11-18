32b287ee00a51037e17a10a0e1f5496bfc90a93f
imanrofiqsy <imrofsy31@gmail.com>
2025-11-18 10:05:47 +0700

    debug_48

--- bms_0n/management/commands/send_data.py
+++ bms_0n/management/commands/send_data.py
@@ -497,7 +497,7 @@ class Command(BaseCommand):
                     for table in tables:
                         for record in table.records:
                             dummy.append({
-                                "time": record.values.get("time"),
+                                "time": record("_time"),
                                 "device": record.values.get("device"),
                                 "value": record.get_value()
                             })
