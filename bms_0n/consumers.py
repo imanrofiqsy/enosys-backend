c005ad8a54c1fdd47a39c97ed33c1178708abb43
imanrofiqsy <imrofsy31@gmail.com>
2025-11-24 10:54:31 +0700

    debug_149

--- bms_0n/consumers.py
+++ bms_0n/consumers.py
@@ -42,7 +42,7 @@ class MyConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             "type": "ping",
             "topic": "ping",
-            "payload": payload
+            "payload": {"event": "A", "topic": "B", "payload": "V"}
         }))
 
     # handler event dari group_send; tipe harus sama: send_dashboard_data
