c6d3f7e766968486518a4d5fe664ebf10f6856a1
imanrofiqsy <imrofsy31@gmail.com>
2025-11-25 10:57:22 +0700

    debug_190

--- bms_0n/management/commands/send_data.py
+++ bms_0n/management/commands/send_data.py
@@ -674,7 +674,7 @@ class Command(BaseCommand):
                     today_start = today()
                     first = 
                     from(bucket: "{BUCKET}")
-                        |> range(start: today(), stop: now())
+                        |> range(start: yesterday_start, stop: today_start)
                         |> filter(fn: (r) => 
                             r._measurement == "power_meter_data" and 
                             r._field == "kwh" and
@@ -686,7 +686,7 @@ class Command(BaseCommand):
 
                     last = 
                     from(bucket: "{BUCKET}")
-                        |> range(start: today(), stop: now())
+                        |> range(start: yesterday_start, stop: today_start)
                         |> filter(fn: (r) => 
                             r._measurement == "power_meter_data" and 
                             r._field == "kwh" and
